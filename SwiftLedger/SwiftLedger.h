1f95210ed8e696b1ccaf8ab2e69bdc7c66d8a991
JeffreyLind22 <jeffrey@jeffreylind.dev>
2024-10-05 10:10:48 -0400

    Initial Commit

---
+++ SwiftLedger/SwiftLedger.h
@@ -0,0 +1,18 @@
+//
+//  SwiftLedger.h
+//  SwiftLedger
+//
+//  Created by Jeffrey Lind on 10/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLedger.
+FOUNDATION_EXPORT double SwiftLedgerVersionNumber;
+
+//! Project version string for SwiftLedger.
+FOUNDATION_EXPORT const unsigned char SwiftLedgerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLedger/PublicHeader.h>
+
+
---
+++ SwiftLedgerTests/SwiftLedgerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLedgerTests.swift
+//  SwiftLedgerTests
+//
+//  Created by Jeffrey Lind on 10/5/24.
+//
+
+import Testing
+@testable import SwiftLedger
+
+struct SwiftLedgerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
