3e9ab463a86e81d78c635aa3f8808b1be1ec4e1a
JeffreyLind22 <jeffrey@jeffreylind.dev>
2024-10-05 10:29:14 -0400

    SwiftLedger 0.0.2: Hello World!

---
+++ SwiftLedger/Ledger.swift
@@ -0,0 +1,14 @@
+//
+//  Ledger.swift
+//  SwiftLedger
+//
+//  Created by Jeffrey Lind on 10/5/24.
+//
+
+public class Ledger {
+    public init() {}
+    
+    public func sayhello() -> String {
+        "Hello from SwiftLedger!"
+    }
+}
